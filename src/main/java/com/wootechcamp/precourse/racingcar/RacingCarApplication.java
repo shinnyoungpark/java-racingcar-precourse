80f7b3a9da3243bcbefaaeaf6d9c9eb6b1493a33
shinyoungpark <shinnyoungpark@gmail.com>
2021-05-01 21:47:22 +0900

    feat(main): add main
    add main
    build structure

---
+++ src/main/java/com/wootechcamp/precourse/racingcar/RacingCarApplication.java
@@ -0,0 +1,11 @@
+package com.wootechcamp.precourse.racingcar;
+
+public class RacingCarApplication {
+
+    public static void main(String[] args){
+
+        RacingCarGame racingCarGame = new RacingCarGame();
+        racingCarGame.start();
+
+    }
+}
---
+++ src/main/java/com/wootechcamp/precourse/racingcar/RacingCarGame.java
@@ -0,0 +1,8 @@
+package com.wootechcamp.precourse.racingcar;
+
+public class RacingCarGame {
+
+    public void start(){
+        System.out.println("Game start!");
+    }
+}
